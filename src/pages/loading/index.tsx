ece8da804edd145614eb8f7cfb32510cae5a338b
Mumimimi <mumichkamu@gmail.com>
2026-05-14 23:00:44 +0300

    fix(loading-page) fixed position

--- src/pages/loading/index.tsx
+++ src/pages/loading/index.tsx
@@ -2,7 +2,11 @@ import 'src/pages/loading/index.css'
 import { JSX } from 'react'
 
 const LoadingPage = (): JSX.Element => {
-  return <span className="loader"></span>
+  return (
+    <div className="w-screen h-screen flex justify-center items-center align-middle">
+      <span className="loader" />
+    </div>
+  )
 }
 
 export default LoadingPage
